59b7e6b1ebe9e2ae520dd4f34669dc26d4da58cc
Vlad Potcebnev <pocvlad@gmail.com>
2023-02-28 23:37:43 +0300

    added HW4

---
+++ src/test/java/SelenideSoftAssertionsCheckTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideSoftAssertionsCheckTest {
+
+    String s = "@ExtendWith({SoftAssertsExtension.class})\n" +
+            "class Tests {\n" +
+            "  @Test\n" +
+            "  void test() {\n" +
+            "    Configuration.assertionMode = SOFT;\n" +
+            "    open(\"page.html\");\n" +
+            "\n" +
+            "    $(\"#first\").should(visible).click();\n" +
+            "    $(\"#second\").should(visible).click();\n" +
+            "  }\n" +
+            "}";
+    @Test
+    void checkAssertionsAndJUnitTest(){
+        open("https://github.com/selenide/selenide");
+        //press button wiki
+        $("#wiki-tab").click();
+        //should have 'SoftAssertions'
+        //click 'SoftAssertions'
+        $("[placeholder='Find a page…']").setValue("SoftAssertions");
+        $(".Layout-sidebar").shouldHave(text("SoftAssertions"));
+        $(byText("SoftAssertions")).click();
+        //should be code example
+        $("#user-content-3-using-junit5-extend-test-class").parent().sibling(0).shouldHave(text(s));
+        sleep(4000);
+
+
+
+    }
+
+
+}
